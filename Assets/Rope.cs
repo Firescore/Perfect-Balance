2316a9fe99d861af5eeaa3734a60c59c2d44a29a
dhiraj7894 <dhiraj7894@gmail.com>
2020-06-08 13:21:24 +0530

    Started repo

---
+++ Assets/Rope.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    List<GameObject> ropeComponents = new List<GameObject>();
+    public List<Rigidbody> rb = new List<Rigidbody>();
+
+    public float mass;
+    public float drag;
+    public float angularDrag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeRigidBody();
+    }
+    void changeRigidBody()
+    {
+        foreach (Transform child in this.transform)
+        {
+            ropeComponents.Add(child.gameObject);
+        }
+
+        for (int i = 0; i < ropeComponents.Count; i++)
+        {
+            rb.Add(ropeComponents[i].GetComponent<Rigidbody>());
+        }
+        for (int i = 0; i < rb.Count; i++)
+        {
+            rb[i].mass = mass;
+            rb[i].drag = drag;
+            rb[i].angularDrag = angularDrag;
+        }
+    }
+}
